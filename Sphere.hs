10ee603405cfffbfe12cdbf182d8d071ce297f2b
Guerric Chupin <guerric.chupin@ensta-paristech.fr>
2015-08-15 23:15:58 +0200

    Modified sphereIntersect so it reports only intersections in front of the origin

--- Sphere.hs
+++ Sphere.hs
@@ -13,20 +13,19 @@ import Color
 data Sphere = Sphere { center :: Point
                      , radius :: Double
                      , color  :: Color
-                     }
+                     } deriving (Eq)
 
 sphereIntersect :: Ray -> Sphere -> [Point]
 sphereIntersect Ray {origin = (a,b,c), dir = (x,y,z)}
                 Sphere {center = (d,e,f), radius = r}
-  | cond > 0 = [(a,b,c) .+ ((-p + rtCond).*normdir),
-                (a,b,c) .+ ((-p - rtCond).*normdir)]
-  | cond == 0 = [(a,b,c) .+ ((-p).*normdir)]
+  | cond > (max 0 p) = [(a,b,c) .+ ((-p + cond).*normdir),
+                        (a,b,c) .+ ((-p - cond).*normdir)]
+  | cond == 0 && p <= 0 = [(a,b,c) .+ ((-p).*normdir)]
   | otherwise = []
   where
     normdir = normalise (x,y,z)
     p = normdir `dotProd` oc
-    cond = p^2 - (sqNorm oc) + r^2
-    rtCond = sqrt cond
+    cond = sqrt $ p^2 - (sqNorm oc) + r^2
     oc = (a,b,c) .- (d,e,f)
 
 instance Renderable Sphere where
@@ -35,4 +34,3 @@ instance Renderable Sphere where
    normal s p = Ray { origin = p
                     , dir    = p .- center s}
    colorAt p s = color s
-
