1342b230b859373933f0aed034a1a383b4b511e4
Olivier Nicole <nicole@ensta.fr>
2015-08-15 14:36:04 +0200

    debug render function

--- Render.hs
+++ Render.hs
@@ -8,7 +8,6 @@ import Pixel
 import qualified Data.Matrix as M
 import GeometricTypes
 import AuxiliaryFunctions
-import Debug.Trace
 
 -- camera is fixed at (0, 0, 1) and the screen is orthogonal to the camera and
 -- is a rectange defined by the two corners (-1, -1, 0) and (1, 1, 0).
@@ -16,11 +15,11 @@ render :: ImageDefinition -> Scene -> Image
 render (w, h) objs = Image $ M.fromList w h $
    map pixelColor
       [Ray {origin = cameraPos,
-            dir    = traceShow (x, y) ((x, y, 0) .- cameraPos)} | x <- abscissas, y <- ordinates]
+            dir    = (x, y, 0) .- cameraPos} | y <- ordinates, x <- abscissas]
    where
    cameraPos = (0, 0, 1)
-   abscissas = [-1 + 2 / fromIntegral w * x | x <- map fromIntegral [0..w]]
-   ordinates = [-1 + 2 / fromIntegral h * y | y <- map fromIntegral [0..h]]
+   abscissas = [-1 + 2 / fromIntegral w * x | x <- map fromIntegral [0..(w - 1)]]
+   ordinates = [-1 + 2 / fromIntegral h * y | y <- map fromIntegral [(h - 1), (h - 2)..0]]
    pixelColor :: Ray -> Pixel
    pixelColor r | or $ map (hit r) objs = white
                 | otherwise             = black
--- test.hs
+++ test.hs
@@ -3,7 +3,9 @@ import Render
 import Shapes
 import Intersection
 
-scene = [MkSceneObject $ Sphere { center = (0, 0, -5), radius = 0.01 }]
+scene = [ MkSceneObject $ Sphere { center = (0, 0, -1), radius = 0.5 }
+        , MkSceneObject $ Sphere { center = (0.8, 0.8, -0.4), radius = 0.2 }
+        ]
 
-main = putStr . show $ render (10, 10) scene
+main = putStr . show $ render (500, 500) scene
 
