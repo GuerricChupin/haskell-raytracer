7121aa373890eb64160113b9004dff58b117866a
Olivier Nicole <nicole@ensta.fr>
2015-08-16 00:30:36 +0200

    clean up test.hs

--- test.hs
+++ test.hs
@@ -4,15 +4,14 @@ import Sphere
 import LightSource
 
 scene = Scene {
-     objs = [ MkSceneObject $ Sphere { center = (0, 0, -10), radius = 6,
-                                       color  = (200, 0, 0) }
-            , MkSceneObject $ Sphere { center = (4, 4, -1), radius = 1,
-                                       color  = (0, 255, 0) }
-            , MkSceneObject $ Sphere { center = (-9,9,-10) , radius = 5,
-                                       color  = (0,0,255) }
-            ]
+     objs = map MkSceneObject spheres
    , source = LightSource { direction = (-1, 1, 0) }
    }
+
+spheres = [ Sphere (0, 0, -10)  6 (200, 0, 0)
+          , Sphere (4, 4, -1)   1 (0, 255, 0)
+          , Sphere (-9, 9, -10) 5 (0, 0, 255)
+          ]
 
 main = putStr . show $ render (1000, 1000) (20, 20) 20 scene
 
