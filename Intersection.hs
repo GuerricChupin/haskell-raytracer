937baa71618c9603eaa6543a3dbb713f050b541b
Guerric Chupin <guerric.chupin@ensta-paristech.fr>
2015-08-15 11:07:54 +0200

    Corrected bug in sphereIntersect

--- Intersection.hs
+++ Intersection.hs
@@ -8,16 +8,16 @@ import GeometricTypes
 sphereIntersect :: Ray -> Sphere -> [Point]
 sphereIntersect Ray {origin = (a,b,c), dir = (x,y,z)}
                 Sphere {center = (d,e,f), radius = r}
-  | p < r = [(a,b,c) .+ ((p + rtCond).*(x,y,z)),
-             (a,b,c) .+ ((p - rtCond).*(x,y,z))]
-  | p == r = [p.*(x,y,z)]
+  | cond > 0 = [(a,b,c) .+ ((-p + rtCond).*normdir),
+                (a,b,c) .+ ((-p - rtCond).*normdir)]
+  | cond == 0 = [p.*(x,y,z)]
   | otherwise = []
   where
-    p = (x,y,z) `dotProd` oc
+    normdir = normalise (x,y,z)
+    p = normdir `dotProd` oc
     cond = p^2 - (sqNorm oc) + r^2
     rtCond = sqrt cond
     oc = (a,b,c) .- (d,e,f)
 
 instance Renderable Sphere where
    hit r s = not $ null $ sphereIntersect r s
-              
