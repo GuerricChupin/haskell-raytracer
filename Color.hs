60fbddddf34c3e7cffc6ab6085f6348002460573
Olivier Nicole <nicole@ensta.fr>
2015-08-16 17:06:13 +0200

    improve 'darken'

--- Color.hs
+++ Color.hs
@@ -15,9 +15,11 @@ white = (255, 255, 255)
 black :: Color
 black = (0, 0, 0)
 
--- darken by the given coefficient between 0 and 1. darken 0 is _ -> black
--- and darken 1 is 'id'.
+-- darken by the given coefficient between 0 and 1. darken x, where
+-- (x <= 0), is _ -> black, darken 1 is 'id' and darken x where x > 1 is
+-- "lighten".
 darken :: Double -> Color -> Color
-darken x (r, g, b) = (f r, f g, f b)
+darken x (r, g, b) | x < 0     = black
+                   | otherwise = (f r, f g, f b)
    where f n = floor (x * fromIntegral n)
 
