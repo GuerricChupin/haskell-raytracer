202bf6996c996af99676e7f4ea05ae2a86520a2d
Olivier Nicole <nicole@ensta.fr>
2015-08-21 11:10:33 +0200

    add Intersection object

---
+++ Intersection.hs
@@ -0,0 +1,23 @@
+module Intersection ( Intersection (Inter)
+                    ) where
+
+import Intersectable
+import Geometry
+import Data.Function (on)
+
+data Intersection a b = Inter a b
+
+instance (Intersectable a, Intersectable b) => Intersectable (Intersection a b) where
+   contains (Inter a b) p = contains a p && contains b p
+   firstIntersection r int@(Inter a b) = case (firstIntersection r a, firstIntersection r b) of
+      (Nothing, Nothing) -> Nothing
+      (Nothing, m'@(Just (p,_,_))) -> if a `contains` p then m' else Nothing
+      (m@(Just (p,_,_)), Nothing) -> if b `contains` p then m else Nothing
+      (i@(Just (p,_,_)), i'@(Just (p',_,_))) -> case (compare `on` distance (origin r)) p p' of
+         LT -> if b `contains` p
+               then i
+               else firstIntersection (r {origin = p}) int
+         _  -> if a `contains` p'
+               then i'
+               else firstIntersection (r {origin = p'}) int
+
--- test.hs
+++ test.hs
@@ -9,25 +9,28 @@ import Chessboard (chessboardShaded)
 import Geometry ((.+), (.-))
 import qualified Geometry as G
 import Color (black, white)
+import Intersection
 
 main =
-  putStr . show $ render (1366 , 768) (27.32, 15.36) 40 scene -- std for testing
+  putStr . show $ render (1366, 768) (27.32, 15.36) 40 scene -- std for testing
   -- putStr . show $ render (1366, 768) (27.32, 15.36) 40 scene -- bigger
 
 scene = Scene {
-   world =   Sphere (rotP (o .+ (-10,0,-20))) 4 `uniform` Mat (200,0,0) 0.3 1.4 0.3
-         ||| Sphere (rotP o) 4 `uniform` Mat (0,200,0) 0.3 1.4 0.3
-         ||| Sphere (rotP (o .+ (10,0,-20))) 4 `uniform` Mat (0,0,200) 0.3 1.4 0.3
-         ||| chessboardShaded Plane { origin = rotP (0, -5, -10)
-                                    , normal = rotV (0, 1, 0)
+   world =   {-Sphere (rotP (o .+ (-10,0,-20))) 4 `uniform` Mat (200,0,0) 0.1 1.4 0.3
+         ||| Sphere (rotP o) 4 `uniform` Mat (0,200,0) 0.1 1.4 0.3
+         ||| Sphere (rotP (o .+ (10,0,-20))) 4 `uniform` Mat (0,0,200) 0.1 1.4 0.3
+         |||-} chessboardShaded Plane { origin = rotP (0, 0, -25)
+                                    , normal = rotV $ (0,0,1)
                                     }
-                              (rotV (1, 0, -0.1)) 5 (Mat white 0 1 1) (Mat black 1 1 1)
+                              (rotV (1, 0, -0.1)) 5 (Mat white 0 1 1) (Mat black 0 1 1)
+         ||| Inter (Sphere (rotP (o .+ (0,0,-r+1))) r) (Sphere (rotP (o .+ (0,0,r-1))) r) `uniform` Mat black 0.1 1.4 0
 
-   , source = LightSource { direction = rotV (1, 1, 1) }
+   , source = LightSource { direction = rotV (1,1,1) }
    }
 
-o = (0, 0, -10)
-angle = pi/12
+r = 25.25
+o = (0, 2, -10)
+angle = pi / 12
 rotV = G.rotateVect (1, 0, 0) angle
 rotP = G.rotatePt (0, 0, -10) (1, 0, 0) angle
 
