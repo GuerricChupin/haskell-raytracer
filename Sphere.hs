3701bc590449bbba1ed6c39bd85ffb00789f742a
Olivier Nicole <nicole@ensta.fr>
2015-12-18 17:34:57 +0100

    Simplify (and hopefully optimize) ray-sphere intersection.

--- Sphere.hs
+++ Sphere.hs
@@ -15,33 +15,19 @@ data Sphere = Sphere { center  :: Point
                      , radius  :: Double
                      } deriving (Eq)
 
-sphereIntersect :: Ray -> Sphere -> [Point]
-sphereIntersect ((a,b,c),(x,y,z),_)
-                Sphere {center = (d,e,f), radius = r}
-  | cond > 0 =
-      [(a,b,c) .+ (ppc.*normdir) | ppc > epsilon] ++
-      [(a,b,c) .+ (pmc.*normdir) | pmc > epsilon]
-  | otherwise = []
-  where
-    normdir = normalise (x,y,z)
-    p = normdir `dotProd` oc
-    cond = sqrt $ p^2 - (sqNorm oc) + r^2
-    oc = (a,b,c) .- (d,e,f)
-    ppc = -p + cond
-    pmc = -p - cond
-
 instance Intersectable Sphere where
    hit r s = isJust $ firstIntersection r s
    contains s p = distance p (center s) < radius s - epsilon
-   firstIntersection ray s
-      | null inters = Nothing
-      | otherwise   = Just $ ( p
-                             , p .- center s
-                             , if rayEnters
-                               then Entering
-                               else Leaving
-                             )
-      where inters = sphereIntersect ray s
-            p = minimumBy (compare `on` (distance (origin ray))) inters
-            o = origin ray
-            rayEnters = (p .- center s) `dotProd` dir ray < epsilon
+   firstIntersection (o, d, _) (Sphere c r)
+      | dist > r  = Nothing
+      | oc > r    = if tc > 0 then Just (p, p .- c, Entering)
+                              else Nothing
+      | otherwise = Just (p', p' .- c, Leaving)
+   where du   = normalise d
+         tc   = (c .- o) `dotProd` du
+         dist = sqrt $ tc^2 - oc^2
+         oc   = distance o c
+         p    = o .+ ((tc - t1c) .* du)
+         p'   = o .+ ((tc + t1c) .* du)
+         t1c  = sqrt $ r^2 - dist^2
+
