c09fecf46cd212ec1b2c5a7f28cc3091e412743d
Guerric Chupin <guerric.chupin@ensta-paristech.fr>
2015-12-18 17:34:40 +0100

    Prettier TestSphere.

--- TestSphere.hs
+++ TestSphere.hs
@@ -15,17 +15,16 @@ import Image
 import Camera
 
 main = render camera scene >>= putStr . show
-       -- render (1366 * 2, 768 * 2) (27.32, 15.36) 40 scene >>= putStr . show
 
 camera :: Camera
 camera =
   fixedAspectRatio (pi/4) $
-  Camera (1366, 768) (25,10,0) (-25,-10,0) (pi/4) (pi,pi) 10
+  Camera (1920, 1080) (25,10,0) (-25,-10,0) 0 (pi,pi) 10
 
 spheres :: [Object]
-spheres =    [Object $ Sphere (0,0,0) 3 `uniform` Mat (255,0,0) 0 1.4 0.5]
-          ++ [Object $ Sphere (-5,0,5) 3 `uniform` Mat (0,255,0) 0 1.4 0.5]
-          ++ [Object $ Sphere (7,0,3) 3 `uniform` Mat (0,0,255) 0 1.4 0.5]
+spheres =    [Object $ Sphere (0,3,0) 3 `uniform` Mat (255,0,0) 0.1 1.4 0.5]
+          ++ [Object $ Sphere (-5,3,5) 3 `uniform` Mat (0,255,0) 0.1 1.4 0.5]
+          ++ [Object $ Sphere (7,3,3) 3 `uniform` Mat (0,0,255) 0.1 1.4 0.5]
           ++ [Object $ chessboardShaded (Plane (0,0,0) (0,1,0)) (1,0,0) 3
                        (Mat (255,255,255) 0.5 1 1)
                        (Mat (0,0,0) 0.5 1 1)]
